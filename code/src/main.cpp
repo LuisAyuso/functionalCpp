f8626df6a2f2205b73e3e419f84cc2989471101f
Luis Ayuso <luis.f.ayuso@gmail.com>
2016-02-14 18:55:14 +0100

    chain more operations

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -10,6 +10,8 @@
 #include <functional>
 #include <vector>
 #include <map>
+#include <list>
+
 
 #include "transform.h"
 #include "filter.h"
@@ -19,55 +21,37 @@
 
 int main(int argc, const char * argv[]) {
     {
+        // lambda in variable.
+        
         std::vector<int> v;
         for (int i =0;i<20;i++) v.push_back(i);
         std::function<int (int)> f = [](int a) -> int { return a+1; };
         
         auto x = func::transform(f, v);
         
-        for(auto it = x.begin(); it != x.end(); ++it){
-            std::cout<< *it << std::endl;
-        }
-        std::cout << "  ======  " << std::endl;
-
         for(auto it : x){
             std::cout<< it << std::endl;
         }
     }
     std::cout << "=============" << std::endl;
     {
+        // inline lambda
+        
         std::vector<int> v;
         for (int i =0;i<15;i++) v.push_back(i);
         
         auto x = func::transform([](int a) -> int { return a-1; }, v);
         
-        for(auto it = x.begin(); it != x.end(); ++it){
-            std::cout<< *it << std::endl;
-        }
         std::cout << "  ======  " << std::endl;
         
         for(auto it : x){
             std::cout<< it << std::endl;
         }
     }
+   
     std::cout << "=============" << std::endl;
     {
-        std::vector<int> v;
-        for (int i =0;i<15;i++) v.push_back(i);
-        
-        auto x = func::transform([](int a) -> int { return a-1; }, v);
-        
-        for(auto it = x.begin(); it != x.end(); ++it){
-            std::cout<< *it << std::endl;
-        }
-        std::cout << "  ======  " << std::endl;
-        
-        for(auto it : x){
-            std::cout<< it << std::endl;
-        }
-    }
-    std::cout << "=============" << std::endl;
-    {
+        // X value
         
         std::function<int (int)> f =[](int x) -> int {return x * 2;};
         auto y = func::transform(f, std::vector<int>({1,2,3,4,5}));
@@ -76,8 +60,11 @@ int main(int argc, const char * argv[]) {
             std::cout<< *it << std::endl;
         }
     }
+    
     std::cout << "=============" << std::endl;
     {
+        // function type
+        
         std::map<int, float> m;
         std::function<int (std::pair<const int,float>)> f =[](std::pair<int, float> x) -> int {return x.first;};
         auto y = func::transform(f,m);
@@ -86,14 +73,50 @@ int main(int argc, const char * argv[]) {
             std::cout<< *it << std::endl;
         }
     }
+    
     std::cout << "=============" << std::endl;
     {
+        // four steps chained.
+        // - variable lambda
+        // - inline lamba
+        // - function type
+        // - functor
+        
+        std::list<int> l = { 0,1,2,3,4,5};
+        
+        auto lambda1 = [](int a) -> int { return a+1; };
+        std::function<int (int)> lambda2 = [](int a) -> int { return a+1; };
+        struct Lambda3{
+            int operator()(int a){
+                return a+1;
+            }
+        } lambda3;
+        
+        auto x = func::transform(lambda1,
+                                 func::transform([](int a){return a+1;},
+                                                 func::transform(lambda2,
+                                                                 func::transform(lambda3, l))));
+      
+        for(int v : x)
+        std::cout << v << std::endl;
+        
+//        std::vector<int> res(x.begin(), x.end());
+    //    std::vector<int> res(l.begin(), l.end());
+        
+    }
+    
+    
+    std::cout << "=============" << std::endl;
+    {
+        // map type
         
         std::map<int, float> m;
-        for (int i =0;i<10;i++)
-                m[i] = i*2.0;
+        for (int i =0;i<10;i++) m[i] = i*2.0;
+        
         std::function<int (float)> f1 = [](float a) ->int { return a+1; };
-        std::function<float(std::pair<int,float>)> f2 =[](std::pair<int, float> x) -> float {return x.second;};
+        std::function<float(std::pair<int,float>)> f2 =[](std::pair<int, float> x) -> float {
+            return x.second;
+        };
         
         auto y = func::transform(f1, func::transform(f2,m));
         for(auto it = y.begin(); it != y.end(); ++it){
@@ -102,8 +125,9 @@ int main(int argc, const char * argv[]) {
     }
     
     std::cout << "=============" << std::endl;
-    // FILTER:
     {
+        // FILTER:
+        
         std::vector<int> v;
         for (int i =0;i<10;i++) v.push_back(i);
         std::function<bool (float)> f = [](int a) ->bool { return a>5; };
@@ -113,6 +137,7 @@ int main(int argc, const char * argv[]) {
             std::cout<< it << std::endl;
     }
     std::cout << "=============" << std::endl;
+    
 //    {
 //        std::vector<int> v;
 //        for (int i =0;i<10;i++) v.push_back(i);
