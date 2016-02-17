02ea93e094cdbeea1a9cddbb81717d5769e966d6
Luis Ayuso <luis.f.ayuso@gmail.com>
2016-02-17 18:21:36 +0100

    dont forget stuff

---
+++ code/include/detail/chaineable.h
@@ -0,0 +1,100 @@
+//
+//  transform.h
+//  functional
+//
+//  Created by Luis Ayuso on 18/02/15.
+//  Copyright (c) 2015 Luis Ayuso. All rights reserved.
+//
+
+#pragma once
+#include <iterator>
+
+#include "utils.h"
+
+namespace func{
+namespace detail{
+    
+    struct Reference_storage{
+    };
+
+    struct Value_storage{
+    };
+
+
+    template<typename C, typename Storage>
+    struct transform_store_t;
+
+    template<typename C>
+    struct transform_store_t<C,Reference_storage>{
+
+        C& storage;
+
+        transform_store_t(C& c)
+        :storage(c) {}
+
+        transform_store_t(transform_store_t&& t)
+        :storage(t.storage) {}
+
+        transform_store_t(const transform_store_t&) = delete;
+    };
+
+    template<typename C>
+    struct transform_store_t<C,Value_storage>{
+
+        C storage;
+
+        transform_store_t(C&& c)
+        :storage(std::move(c)) {}
+
+        transform_store_t(transform_store_t&& t)
+        :storage(std::move(t.storage)) {}
+
+        transform_store_t(const transform_store_t&) = delete;
+    };
+    
+    template<typename N,                // container element type
+             typename R,                // output element type
+             typename C,                // container type
+             typename Storage_type,     // kind of storage (reference or value)
+             typename Iterator          // what kind of iterator this chaineable will provide
+                 > 
+    struct chaineable_t{
+
+        static_assert(validate_container_function<N, C>::value,
+                      "the function does not accept the collection type as paramenter");
+        
+        using value_type = R;
+        using function_type = std::function<R (N)>;
+        using storage_t = detail::transform_store_t<C, Storage_type>;
+
+        function_type func;
+        storage_t store;
+
+        chaineable_t(function_type func, storage_t&& store) : func(func), store(std::move(store)) {}
+        
+        using iterator = Iterator;
+        using const_iterator = const Iterator;
+        
+        chaineable_t (const chaineable_t & ) = delete;
+        chaineable_t (chaineable_t&& o) :func(o.func), store(std::move(o.store)) {}
+        
+        iterator begin(){
+            return iterator(func, store.storage.begin(), store.storage.end());
+        }
+        
+        iterator end(){
+            return iterator(func, store.storage.end(), store.storage.end());
+        }
+        
+        const_iterator begin() const{
+            return const_iterator(func, store.storage.begin());
+        }
+        
+        const_iterator end() const{
+            return const_iterator(func, store.storage.end());
+        }
+    };
+
+}// detail namespace
+}// func namespace
+
