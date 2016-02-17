bd014673bf77c0c29593e242601597bf75da82f0
Luis Ayuso <luis.f.ayuso@gmail.com>
2016-02-17 11:41:05 +0100

    refactor transform, all green

--- code/include/transform.h
+++ code/include/transform.h
@@ -10,45 +10,44 @@
 #include <iterator>
 
 #include "utils.h"
+#include "detail/chaineable.h"
 
 namespace func{
     
     template<typename Value, typename Source, typename Func>
-    struct m_iterator : public std::iterator<std::input_iterator_tag, Value>{
+    struct TransforIterator : public std::iterator<std::input_iterator_tag, Value>{
 
         Func f;
         Source s;
         
-        using self_type = m_iterator<Value, Source, Func>;
+        using self_type = TransforIterator<Value, Source, Func>;
 
-        //m_iterator():f(), s(){}
-        
-        m_iterator(Func f, const Source s)
+        TransforIterator(Func f, const Source s)
         :f(f), s(s) {}
         
         template <typename A, typename B, typename F>
-        m_iterator(const m_iterator<A,B,F>& o)
+        TransforIterator(const TransforIterator<A,B,F>& o)
         :f(o.f), s(o.s){
             static_assert(std::is_same<A, Value>::value,  "incompatible iterators");
             static_assert(std::is_same<B, Source>::value, "incompatible iterators");
         }
         
         template <typename A, typename B, typename F>
-        m_iterator(m_iterator<A,B,F>&& o)
+        TransforIterator(TransforIterator<A,B,F>&& o)
         :f(o.f), s(o.s){
             static_assert(std::is_same<A, Value>::value,  "incompatible iterators");
             static_assert(std::is_same<B, Source>::value, "incompatible iterators");
         }
         
         template <typename A, typename B, typename F>
-        bool operator == (const m_iterator<A,B,F>& o){
+        bool operator == (const TransforIterator<A,B,F>& o){
             static_assert(std::is_same<A, Value>::value,  "incompatible iterators");
             static_assert(std::is_same<B, Source>::value, "incompatible iterators");
             return this->s == o.s;
         }
         
         template <typename A, typename B, typename F>
-        bool operator != (const m_iterator<A,B,F>& o){
+        bool operator != (const TransforIterator<A,B,F>& o){
             static_assert(std::is_same<A, Value>::value,  "incompatible iterators");
             static_assert(std::is_same<B, Source>::value, "incompatible iterators");
             return s != o.s;
@@ -72,119 +71,46 @@ namespace func{
         }
     };
 
-    struct Reference_storage{
-    };
 
-    struct Value_storage{
-    };
+   template <typename N, typename R, typename C, typename Storage_type> 
+    using transform_t = detail::chaineable_t<
+                            N, R,C,Storage_type, // forward paramenters
+                            TransforIterator<R,typename std::remove_reference<C>::type::iterator,std::function<R(N)>> // specific iterator type for transformation
+                                >;
 
-    namespace detail{
-
-        template<typename C, typename Storage>
-        struct transform_store_t;
-
-        template<typename C>
-        struct transform_store_t<C,Reference_storage>{
-
-            C& storage;
-
-            transform_store_t(C& c)
-            :storage(c) {}
-
-            transform_store_t(transform_store_t&& t)
-            :storage(t.storage) {}
-
-            transform_store_t(const transform_store_t&) = delete;
-        };
-
-        template<typename C>
-        struct transform_store_t<C,Value_storage>{
-
-            C storage;
-
-            transform_store_t(C&& c)
-            :storage(std::move(c)) {}
-
-            transform_store_t(transform_store_t&& t)
-            :storage(std::move(t.storage)) {}
-
-            transform_store_t(const transform_store_t&) = delete;
-        };
-
-    }// detail namespace
-
-
-    
-    template<typename N, typename R, typename C, typename Storage_type>
-    struct transform_t{
-
-        static_assert(validate_container_function<N, C>::value,
-                      "the function does not accept the collection type as paramenter");
-        
-        using value_type = R;
-        using function_type = std::function<R (N)>;
-        using storage_t = detail::transform_store_t<C, Storage_type>;
-
-        function_type func;
-        storage_t store;
-
-        transform_t(function_type func, storage_t&& store) : func(func), store(std::move(store)) {}
-        
-        using iterator = m_iterator<R, typename std::remove_reference<C>::type::iterator, std::function<R (N)>>;
-        using const_iterator = const m_iterator<R, typename std::remove_reference<C>::type::iterator, std::function<R (N)>>;
-        
-        transform_t (const transform_t & ) = delete;
-        transform_t (transform_t&& o) :func(o.func), store(std::move(o.store)) {}
-        
-        iterator begin(){
-            return iterator(func, store.storage.begin());
-        }
-        
-        iterator end(){
-            return iterator(func, store.storage.end());
-        }
-        
-        const_iterator begin() const{
-            return const_iterator(func, store.storage.begin());
-        }
-        
-        const_iterator end() const{
-            return const_iterator(func, store.storage.end());
-        }
-    };
 
     // for function type
     // lvalue collection
     template <typename N, typename R, typename C>
-    transform_t<N,R,C,Reference_storage> transform(std::function<R (N)> f, C& c){
-        return transform_t<N,R,C,Reference_storage> (f, detail::transform_store_t<C,Reference_storage> (c));
+    transform_t<N,R,C,detail::Reference_storage> transform(std::function<R (N)> f, C& c){
+        return transform_t<N,R,C,detail::Reference_storage> (f, detail::transform_store_t<C,detail::Reference_storage> (c));
     }
 
     // for function type
     // rvalue collection
     template <typename N, typename R, typename C>
-    transform_t<N,R,C,Value_storage> transform(std::function<R (N)> f, C&& c){
-        return transform_t<N,R,C,Value_storage> (f, detail::transform_store_t<C,Value_storage> (std::move(c)));
+    transform_t<N,R,C,detail::Value_storage> transform(std::function<R (N)> f, C&& c){
+        return transform_t<N,R,C,detail::Value_storage> (f, detail::transform_store_t<C,detail::Value_storage> (std::move(c)));
     }
 
     // for lambda type
     // lvalue collection
     template <typename F, typename C>
-    transform_t<typename get_lambda<F,C>::param_type, typename get_lambda<F,C>::return_type, C, Reference_storage>
+    transform_t<typename get_lambda<F,C>::param_type, typename get_lambda<F,C>::return_type, C, detail::Reference_storage>
     transform(F f, C& c){
         using N = typename get_lambda<F,C>::param_type;
         using R = typename get_lambda<F,C>::return_type;
-        return transform_t<N,R,C,Reference_storage> (f, detail::transform_store_t<C,Reference_storage> (c));
+        return transform_t<N,R,C,detail::Reference_storage> (f, detail::transform_store_t<C,detail::Reference_storage> (c));
     }
 
     // for lambda type
     // xvalue collection
     template <typename F, typename C>
-    transform_t<typename get_lambda<F,C>::param_type, typename get_lambda<F,C>::return_type, C, Value_storage>
+    transform_t<typename get_lambda<F,C>::param_type, typename get_lambda<F,C>::return_type, C, detail::Value_storage>
     transform(F f, C&& c){
         using N = typename get_lambda<F,C>::param_type;
         using R = typename get_lambda<F,C>::return_type;
-        return transform_t<N,R,C,Value_storage> (f, detail::transform_store_t<C,Value_storage> (std::move(c)));
+        return transform_t<N,R,C,detail::Value_storage> (f, detail::transform_store_t<C,detail::Value_storage> (std::move(c)));
     }
 }
 
--- code/tests/ut_intrinsics_transform.cc
+++ code/tests/ut_intrinsics_transform.cc
@@ -20,7 +20,7 @@ TEST(Transform_t, ctor_ref){
     std::function< int(int)> func = [] (int i) { return i; };
     std::vector<int> vec = {1,2,3,4,5,6};
 
-    transform_t<int,int,std::vector<int>,Reference_storage>  x(func, detail::transform_store_t<std::vector<int>, Reference_storage>(vec));
+    transform_t<int,int,std::vector<int>,detail::Reference_storage>  x(func, detail::transform_store_t<std::vector<int>, detail::Reference_storage>(vec));
 
     std::vector<int> res (x.begin(), x.end());
     EXPECT_THAT(res, ElementsAre(1,2,3,4,5,6));
@@ -31,7 +31,7 @@ TEST(Transform_t, ctor_val){
     std::function< int(int)> func = [] (int i) { return i; };
     std::vector<int> vec = {2,2,3,3,5,6};
 
-    transform_t<int,int,std::vector<int>,Value_storage>  x(func, detail::transform_store_t<std::vector<int>, Value_storage>(std::move(vec)));
+    transform_t<int,int,std::vector<int>,detail::Value_storage>  x(func, detail::transform_store_t<std::vector<int>, detail::Value_storage>(std::move(vec)));
 
     std::vector<int> res (x.begin(), x.end());
     EXPECT_THAT(res, ElementsAre(2,2,3,3,5,6));
@@ -43,10 +43,10 @@ TEST(Transform_t, chain_ref){
     std::function< int(int)> func2 = [] (int i) { return 2*i; };
     std::vector<int> vec = {1,1,2,2};
 
-    transform_t<int,int,std::vector<int>,Reference_storage>  x(func1, detail::transform_store_t<std::vector<int>, Reference_storage>(vec));
+    transform_t<int,int,std::vector<int>,detail::Reference_storage>  x(func1, detail::transform_store_t<std::vector<int>, detail::Reference_storage>(vec));
     using in_t = decltype (x);
 
-    transform_t<int,int, in_t, Reference_storage>  y(func2, x);
+    transform_t<int,int, in_t, detail::Reference_storage>  y(func2, x);
 
     std::vector<int> res (y.begin(), y.end());
     EXPECT_THAT(res, ElementsAre(4,4,6,6));
@@ -57,9 +57,9 @@ TEST(Transform_t, chain_val){
     std::function< int(int)> func1 = [] (int i) { return 1+i; };
     std::function< int(int)> func2 = [] (int i) { return 2*i; };
 
-    using inside_t = transform_t<int,int,std::vector<int>,Value_storage>;
+    using inside_t = transform_t<int,int,std::vector<int>,detail::Value_storage>;
         
-    transform_t<int,int, inside_t, Value_storage>  x(func2, inside_t(func1, detail::transform_store_t<std::vector<int>, Value_storage>(std::vector<int>{{2,2,1,1}})));
+    transform_t<int,int, inside_t, detail::Value_storage>  x(func2, inside_t(func1, detail::transform_store_t<std::vector<int>, detail::Value_storage>(std::vector<int>{{2,2,1,1}})));
 
     std::vector<int> res (x.begin(), x.end());
     EXPECT_THAT(res, ElementsAre(6,6,4,4));
