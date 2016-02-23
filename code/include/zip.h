505199b98b14cb1ad2178501868875e6bc4fe540
stefanmoosbrugger <stefanmoosbrugger@gmail.com>
2016-02-23 17:42:18 +0100

    implementing zip infrastructure, test cases broken

--- code/include/zip.h
+++ code/include/zip.h
@@ -8,11 +8,156 @@
 
 #pragma once
 #include <iterator>
+#include <cassert>
 
 #include "detail/utils.h"
 #include "detail/chaineable.h"
 
 namespace func{
+    namespace {
+        template <typename... Args>
+        struct get_value_type {
+            using type = std::tuple<Args...>;
+        };
 
+        template <typename T>
+        struct get_value_type<T> {
+            using type = T;
+        };
+
+        template <typename T, typename V>
+        struct get_value_type<T,V> {
+            using type = std::pair<T,V>;
+        };
+
+        template <typename... Args>
+        using get_value_type_t = typename get_value_type<Args...>::type;
+
+        /*************GET TUPLE ELEMENT***************/
+
+        template <typename Iter, typename T, unsigned N>
+        struct aux {
+            template <typename... Args>
+            static Iter get_begin(T& tuple, Args... storages) {
+                return aux<Iter,T,N-1>::get_begin(tuple, std::get<N-1>(tuple).begin(), storages...);
+            }
+            template <typename... Args>
+            static Iter get_end(T& tuple, Args... storages) {
+                return aux<Iter,T,N-1>::get_begin(tuple, std::get<N-1>(tuple).end(), storages...);
+            }
+        };
+
+        template <typename Iter, typename T>
+        struct aux<Iter,T,0> {
+            template <typename... Args>
+            static Iter get_begin(T& tuple, Args... storages) {
+                return Iter(typename Iter::inner_type(storages...));
+            }
+            template <typename... Args>
+            static Iter get_end(T& tuple, Args... storages) {
+                return Iter(typename Iter::inner_type(storages...));
+            }
+        };
+
+        /*************TRANSFORM TUPLE***************/
+        template <typename T1, typename T2, unsigned N>
+        struct trf {
+            static void apply_pp(T1& input, T2& output) {
+                std::get<N>(output) = ++std::get<N>(input);
+                trf<T1,T2,N-1>::apply_pp(input, output);
+            }
+            static void apply_ind(T1& input, T2& output) {
+                std::get<N>(output) = *std::get<N>(input);
+                trf<T1,T2,N-1>::apply_ind(input, output);
+            }
+        };
+
+        template <typename T1, typename T2>
+        struct trf<T1,T2,0> {
+            static void apply_pp(T1& input, T2& output) {
+                std::get<0>(output) = ++std::get<0>(input);
+            }
+            static void apply_ind(T1& input, T2& output) {
+                std::get<0>(output) = *std::get<0>(input);
+            }
+        };
+
+    }
+
+    // iterator
+    template <typename T, typename Value>
+    struct ZipIterator : public std::iterator<std::input_iterator_tag, Value> {
+        T source;
+        using inner_type = T;
+        using value_type = Value;
+
+        ZipIterator(const T& s) : source(s) { }
+        ZipIterator(T&& s) : source(std::move(s)) { }
+        ZipIterator(ZipIterator&& o) : source(std::move(o.source)) { }
+        ZipIterator(const ZipIterator& o) : source(o.source) { }
+        ZipIterator& operator=(const ZipIterator& s) = delete;
+        ZipIterator& operator=(ZipIterator&& s) = delete;
+
+        bool operator== (const ZipIterator& o) const {
+            return source == o.source;
+        }
+
+        bool operator!= (const ZipIterator& o) const {
+            return source != o.source;
+        }
+
+        ZipIterator operator++() {
+            ZipIterator cpy = *this;
+            trf<inner_type, inner_type, std::tuple_size<inner_type>::value-1>::apply_pp(source, source);
+            return cpy;
+        }
+
+        ZipIterator& operator++(int) {
+            trf<inner_type, inner_type, std::tuple_size<inner_type>::value-1>::apply_pp(source, source);
+            return *this;
+        }
+
+        value_type operator*() {
+            value_type vals;
+            trf<inner_type, value_type, std::tuple_size<inner_type>::value-1>::apply_ind(source, vals);
+            return vals;
+        }
+
+        value_type operator->() {
+            value_type vals;
+            trf<inner_type, value_type, std::tuple_size<inner_type>::value-1>::apply_ind(source, vals);
+            return vals;
+        }
+    };
+
+
+    // chainable
+    template <typename... Args>
+    struct zip_t {
+        // tuple of given container value types
+        using value_type = get_value_type_t<typename Args::value_type...>;
+        std::tuple<Args...> storage;
+
+        using inner_iterator_type = get_value_type_t<typename Args::iterator...>;
+        using iterator = ZipIterator<inner_iterator_type, value_type>;
+
+        zip_t(Args... containers) : storage(containers...) { }
+
+        iterator begin() {
+            return aux<iterator, decltype(storage), sizeof...(Args)>::get_begin(this->storage);
+        }
+
+        iterator end() {
+            return aux<iterator, decltype(storage), sizeof...(Args)>::get_end(this->storage);
+        }
+    };
+
+    // function
+    /*
+    template <typename... Args>
+    zip_t<> zip(Args... a) {
+
+    }
+    */
 }
 
--- code/tests/ut_zip.cc
+++ code/tests/ut_zip.cc
@@ -14,19 +14,56 @@
 
 using namespace testing;
 
-TEST(Transform, lambda1){
-        // lambda in variable.
-        std::vector<int> a;
-        std::vector<int> b;
-
-       for (int i =0;i<10;i++) {
-            a.push_back(i);
-            b.push_back(i);
-       }
-
-       // auto x = func::zip(a, b);
-
-       // std::vector<int> res (x.begin(), x.end());
-       // EXPECT_THAT(res, ElementsAre(1,2,3,4,5,6,7,8,9,10));
+TEST(Zip, traits){
+    EXPECT_TRUE((std::is_same<int, func::zip_t<std::vector<int>>::value_type>::value));
+    EXPECT_TRUE((std::is_same<std::pair<int,float>, func::zip_t<std::vector<int>, std::vector<float>>::value_type>()));
+    EXPECT_TRUE((std::is_same<std::tuple<int,float,double>, func::zip_t<std::vector<int>, std::vector<float>, std::vector<double>>::value_type>()));
 }
+
+TEST(Zip, iterator){
+    std::vector<int> a = {1,2,3,4};
+    std::vector<float> b = {.1,.2,.3,.4};
+
+    auto y = func::zip_t<std::vector<int>,std::vector<float>>(a,b);
+    auto z = y.begin();
+    auto zz = y.end();
+    EXPECT_EQ(std::get<0>(z.source), a.begin());
+    EXPECT_EQ(std::get<1>(z.source), b.begin());
+    EXPECT_EQ(std::get<0>(zz.source), a.end());
+    EXPECT_EQ(std::get<1>(zz.source), b.end());
+
+    EXPECT_EQ(z.source, std::make_pair(a.begin(), b.begin()));
+
+    EXPECT_EQ(*(std::get<0>(z.source)), 1);
+    EXPECT_FLOAT_EQ(*(std::get<1>(z.source)), 0.1);
+
+    ++z;
+    EXPECT_EQ(*(std::get<0>(z.source)), 2);
+    EXPECT_FLOAT_EQ(*(std::get<1>(z.source)), 0.2);
+
+}
+
+TEST(Zip, iterator1){
+    std::vector<int> a = {1,2,3,4};
+    std::vector<float> b = {.1,.2,.3,.4};
+
+    auto y = func::zip_t<std::vector<int>,std::vector<float>>(a,b);
+    const auto& z = y.begin();
+    EXPECT_EQ(*(std::get<0>(z.source)), *a.begin());
+    EXPECT_EQ(*(std::get<1>(z.source)), *b.begin());
+
+    std::vector<std::pair<int,float>> lp;
+    std::copy(y.begin(), y.end(), std::back_inserter(lp));
+    EXPECT_EQ(lp[0].first, 1);
+    EXPECT_EQ(lp[0].second, 0.1);
+    EXPECT_EQ(lp[1].first, 2);
+    EXPECT_EQ(lp[1].second, 0.2);
+    EXPECT_EQ(lp[2].first, 3);
+    EXPECT_EQ(lp[2].second, 0.3);
+    EXPECT_EQ(lp[3].first, 4);
+    EXPECT_EQ(lp[3].second, 0.4);
+
+}
+
+
 
